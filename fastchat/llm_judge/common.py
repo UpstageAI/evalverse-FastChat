b153e64f1554bbe239cafd8508f19ad83790c619
choco9966 <choco_9966@naver.com>
2024-07-29 03:24:55 +0900

    add organization format

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -405,23 +405,28 @@ def play_a_match_pair(match: MatchPair, output_file: str):
 
 
 def chat_completion_openai(model, conv, temperature, max_tokens, api_dict=None):
+    from openai import OpenAI
     if api_dict is not None:
         openai.api_base = api_dict["api_base"]
-        openai.api_key = api_dict["api_key"]
+        openai.api_key = 'api_dict["api_key"]'
+    client = OpenAI(
+        organization=os.environ["OPENAI_ORGANIZATION"],
+        project=os.environ["OPENAI_PROJECT"],
+    )
     output = API_ERROR_OUTPUT
     for _ in range(API_MAX_RETRY):
         try:
             messages = conv.to_openai_api_messages()
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model=model,
                 messages=messages,
                 n=1,
                 temperature=temperature,
                 max_tokens=max_tokens,
             )
-            output = response["choices"][0]["message"]["content"]
+            output = response.choices[0].message.content
             break
-        except openai.error.OpenAIError as e:
+        except openai.OpenAIError as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
 
